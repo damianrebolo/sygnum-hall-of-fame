47331ab45b00ffc92b258c1a4ba32bfb22dd2412
Damian Rebolo <damianrebolo@gmail.com>
2023-02-26 19:39:13 +0100

    feat: Create alert component

---
+++ src/components/Alert.js
@@ -0,0 +1,5 @@
+export const Alert = ({ children }) => (
+  <div className="flex justify-center items-center w-full h-20 bg-gradient-to-r from-rose-700 via-violet-500 to-sky-300">
+    {children}
+  </div>
+);
