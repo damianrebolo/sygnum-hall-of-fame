a29ee3dd9c3f97712852db038ea4c81a5c03a58f
Damian Rebolo <damianrebolo@gmail.com>
2023-07-18 15:27:19 +0200

    feature: vite config build output

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     nodePolyfills({
       // To exclude specific polyfills, add them to this list.
       exclude: [
-        'fs', // Excludes the polyfill for `fs` and `node:fs`.
+        "fs", // Excludes the polyfill for `fs` and `node:fs`.
       ],
       // Whether to polyfill specific globals.
       globals: {
@@ -19,5 +19,9 @@ export default defineConfig({
       },
       // Whether to polyfill `node:` protocol imports.
       protocolImports: true,
-    }),],
-})
+    }),
+  ],
+  build: {
+    outDir: "build",
+  },
+});
